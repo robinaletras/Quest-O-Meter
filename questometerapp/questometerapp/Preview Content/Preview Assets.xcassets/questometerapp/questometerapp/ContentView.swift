b9f6b430830d3b53465ed3274ca24a75a516e592
Robin Aletras <robinaletras@gmail.com>
2024-04-22 00:18:29 -0400

    Initial Commit

---
+++ questometerapp/questometerapp/Preview Content/Preview Assets.xcassets/questometerapp/questometerapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  questometerapp
+//
+//  Created by Robin Aletras on 4/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ questometerapp/questometerapp/questometerappApp.swift
@@ -0,0 +1,17 @@
+//
+//  questometerappApp.swift
+//  questometerapp
+//
+//  Created by Robin Aletras on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct questometerappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
